587ea1186d7f21e8c24d5f020233b8f394d601d1
Freedom Loisa <kitakayaloisa@gmail.com>
2024-12-02 15:27:05 +0300

    helper functions

---
+++ src/utils/secrets.js
@@ -0,0 +1,4 @@
+export const backend = import.meta.env.VITE_BACKEND;
+export const companyWebsite = import.meta.env.VITE_COMPANY_WEBSITE;
+export const geoApifyApiKey = import.meta.env.VITE_GEOAPIFY_API_KEY;
+export const mapTilerApiKey = import.meta.env.VITE_MAP_TILER_API_KEY;
